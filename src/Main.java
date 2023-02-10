5c9ea0d2ecf52832383b771ad3788e9804639ad7
bykovkaya <bykovskaya_01@mail.ru>
2023-02-10 19:37:46 +0200

    Redone homework 11-25.01

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         isYearLeap(2021);
-        determineOSVersion(0, 2015);
+        determineOSVersion(0, 2014);
         determineOSVersion(1, 2023);
         int time1 = calculateTime(50);
         int time2 = calculateTime(100);
+        int time3 = calculateTime(101);
         System.out.println("Для дистанции 50 время = " + time1);
         System.out.println("Для дистанции 100 время = " + time2);
+        System.out.println("Для дистанции свыше 100 - доставки нет");
     }
 
     public static void isYearLeap(int year) {
@@ -20,30 +23,30 @@ public class Main {
         }
     }
 
-    public static void determineOSVersion(int osType, int deviceYear) {
+    public static void determineOSVersion(int clientOS, int deviceYear) {
         System.out.println("Задача 2");
         int currentYear = LocalDate.now().getYear();
-        if (osType == 0) {
-            if (deviceYear == currentYear) {
-                System.out.println("Устанавливаем облегченную iOS");
-            } else {
-                System.out.println("Устанавливаем обычную iOS");
-            }
-        } else {
-            if (deviceYear == currentYear) {
-                System.out.println("Устанавливаем облегченную android");
-            } else {
-                System.out.println("Устанавливаем обычную android");
-            }
+        if (clientOS == 0 && deviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && deviceYear > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         }
-    }
-    public static int calculateTime (int distance) {
+        if (clientOS == 1 && deviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && deviceYear > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
         System.out.println("Задача 3");
+    }
+    public static int calculateTime(int distance) {
         int time = 1;
         if (distance <= 20 && distance <= 60) {
             ++time;
         }
         if (distance > 60 && distance <= 100) {
+            ++time;
+        }
+        if (distance > 100) {
             ++time;
         }
         return time;
